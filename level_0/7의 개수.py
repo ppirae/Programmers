bf704d8904e857824edead020a5ef6ac2445cb76
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-10-11 21:50:39 +0900

    Programmers

---
+++ level_0/7의 개수.py
@@ -0,0 +1,5 @@
+def solution(array):
+    answer = 0
+    array = ''.join(str(array))
+    answer = array.count('7')
+    return answer
---
+++ level_0/A로 B 만들기.py
@@ -0,0 +1,4 @@
+def solution(before, after):
+    b = sorted(before)
+    a = sorted(after)
+    return 1 if b == a else 0
